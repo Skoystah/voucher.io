177d5754f23ebc333a0bec5e1c85c54aa209ce4b
Skoystah <geert.dooms@gmail.com>
2025-08-09 15:15:35 +0200

    oops

--- src/voucher/db.py
+++ src/voucher/db.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from sqlalchemy import Boolean, CheckConstraint, String, create_engine, select
-from sqlalchemy.exc import DatabaseError, IntegrityError
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Session, mapped_column, Mapped
 
 class Base(MappedAsDataclass, DeclarativeBase):
